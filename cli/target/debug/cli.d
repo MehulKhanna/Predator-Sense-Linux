8710d65068be725787611f354d9038a58cd6e9da
MehulKhanna <mehulkhanna420@gmail.com>
2025-07-15 22:48:11 +0530

    Init Tauri + React + Shadcn

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+/home/mehul/Documents/Predator-Sense-Linux/cli/target/debug/cli: /home/mehul/Documents/Predator-Sense-Linux/cli/src/main.rs
---
+++ cli/target/debug/deps/cli-d921c99c15faea1e.d
@@ -0,0 +1,5 @@
+/home/mehul/Documents/Predator-Sense-Linux/cli/target/debug/deps/cli-d921c99c15faea1e.d: src/main.rs
+
+/home/mehul/Documents/Predator-Sense-Linux/cli/target/debug/deps/libcli-d921c99c15faea1e.rmeta: src/main.rs
+
+src/main.rs:
